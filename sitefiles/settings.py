c27a4b2f08558a442ad4115d6bf6631bc572f414
minjeong <mjkimdelta@gmail.com>
2021-03-07 22:59:02 +0900

    Modify root strings and other settings

--- sitefiles/settings.py
+++ sitefiles/settings.py
@@ -35,7 +35,7 @@ def get_secret(setting, secrets=secrets): #예외 처리를 통해 오류 발생
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
