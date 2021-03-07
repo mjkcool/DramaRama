f76d860a0864d189a5a0ddb46b715b3a67f765fa
minjeong <mjkimdelta@gmail.com>
2021-03-07 12:00:03 +0900

    Create and restore project

---
+++ dramaramaapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Drama)
+admin.site.register(Survey)
+admin.site.register(R_Survey)
---
+++ dramaramaapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.conf.urls import include, url
+from . import views
+
+
+# app_name = 'dramaramaapp'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('Who-you-are', views.form, name='form'),
+    path('To.you--From.Rama💜', views.result),
+    path('your-answer', views.answer),
+    path('share-your-joy!', views.survey, name='survey'),
+    path('submit', views.submitSurvey, name='submit'),
+]
